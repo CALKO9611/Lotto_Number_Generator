536adb01fce5359e527f9a5da9b07aa01d2fe907
CALKO9611 <kimd9669@naver.com>
2025-06-02 18:40:38 +0900

    ui 파일 추가 및 파일 이름 수정

--- root.py
+++ root.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from ui import setup_ui
 
 root = Tk()
 root.title("로또 번호 생성기")
 root.resizable(False, False)
+
+# ui
+setup_ui(root)
 
 root.mainloop()
---
+++ ui.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+
+def setup_ui(root):
+    return
