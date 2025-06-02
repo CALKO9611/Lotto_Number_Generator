087c8ecd3ec73953158dc8941e5b894371c9bdc0
CALKO9611 <kimd9669@naver.com>
2025-06-02 18:43:31 +0900

    소개 문구 프레임 추가

--- ui.py
+++ ui.py
@@ -2,4 +2,12 @@ from tkinter import *
 
 
 def setup_ui(root):
-    return
+    # 소개 문구 프레임
+    intro_frame = Frame(root)
+    intro_frame.pack(fill="both")
+
+    intro_label = Label(
+        intro_frame,
+        text="로또 번호 자동 추출 프로그램입니다.\n사용 방법은 '도움말' 버튼을 눌러 확인해주세요.",
+    )
+    intro_label.pack()
