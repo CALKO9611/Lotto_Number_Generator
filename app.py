bba319a5a96f4b3f0b3f5b2422eb5b0c104e707f
CALKO9611 <kimd9669@naver.com>
2025-07-09 19:12:16 +0900

    pyintaller 사용하기 위한 ico 경로 지정

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import tkinter as tk
+import sys
+import os
 from layout import setup_main_ui
 
 root = tk.Tk()
 root.title("로또 번호 생성기")
 root.resizable(False, False)
-root.iconbitmap("icon32.ico")
+
+# 아이콘 경로 설정
+if getattr(sys, "frozen", False):
+    # exe로 실행될 때
+    base_path = sys._MEIPASS
+else:
+    # python으로 실행할 때
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+icon_path = os.path.join(base_path, "icon32.ico")
+root.iconbitmap(icon_path)
 
 # ui
 setup_main_ui(root)
